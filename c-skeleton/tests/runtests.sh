c7e8b676b6e60d4f0e20845879e4493d9d959b7b
Steven Troxler <steven.troxler@berkeleyanalytics.com>
2015-05-18 01:12:26 -0400

    Add c-skeleton, ex28

---
+++ c-skeleton/tests/runtests.sh
@@ -0,0 +1,30 @@
+echo "Running unit tests"
+
+# create a header for this run in the log
+echo "\n" >> tests/tests.log
+echo "Starting Test Suite "$(date) >> tests/tests.log
+echo "================================================" >> tests/tests.log
+
+for tf in tests/*_tests; do
+
+    # Create a header for this file in the log
+    echo "" >> tests/tests.log
+    echo "RUNNING $tf" >> tests/tests.log
+    echo "----------------------------"
+
+    if test -f $tf; then
+        # run the test, adding to log, and if exit code is non-zero
+        # give an error message & abort.
+        if $VALGRIND ./tf 2 >> tests/tests.log; then
+            echo "$tf PASS"
+        else
+            echo "ERROR in test $tf! Here's tests/test.log:"
+            tail tests/tests.log
+            exit 1  # I'm not sure I agree with this... sometimes it's useful
+                    # to see a full set of which tests failed in a regression.
+        fi
+    fi
+done
+
+echo ""
+
