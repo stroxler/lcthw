5f855102b359b99d91264ebd932c888202d0620c
Steven Troxler <steven.troxler@berkeleyanalytics.com>
2015-05-19 13:11:22 -0400

    Add example 31, a very simple gdb demo

---
+++ ex31/exe.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "dbg.h"
+
+int main(int argc, char* argv[]) {
+    int i = 0;
+    while (i < 100) {
+        usleep(3000);
+    }
+    return 0;
+}
